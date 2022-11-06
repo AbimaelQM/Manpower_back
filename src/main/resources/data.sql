3d78e9f2a5b00ecf5c50a53efa5d57104e7b0efe
AbimaelQM <60951913+AbimaelQM@users.noreply.github.com>
2022-11-06 10:41:19 -0500

    Deploy heroku data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,14 +1,14 @@
 
 
 -- id, nome
-INSERT INTO area (id,name) VALUES 
+INSERT INTO area (id,name) VALUES aaaa
 (1,'Artificial Intelligence'),
 (2,'Data Analysis'),
 (3,'SW Configuration Management'),
 (4,'UI/UX Customer Experience '),
 (5,'SW Development'),
 (6,'Testing'),
-(7,'Project Management');
+(7,'Project Management')
 
 INSERT INTO skill (id,name,area_id) VALUES 
 (1,'Cloud Big Data Engineer',1),
