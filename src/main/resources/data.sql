b930c67214d948a7c747c4523d619593e970e2d9
AbimaelQM <60951913+AbimaelQM@users.noreply.github.com>
2022-11-06 10:27:12 -0500

    Deploy heroku data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -54,10 +54,10 @@ INSERT INTO skill (id,name,area_id) VALUES
 (41,'Project Lead',7);
 
 INSERT INTO usuario (id,active,email,name,role,password) VALUES 
-(1,1,'admin@gmail.com','Admin','ROLE_ADMIN','$2a$10$zArhMpAXoIhDRvqmJSPI2uDJbGxoQft9g2LCa.7fBCOXDuqKVC6cS'),
-(2,1,'vic@gmail.com','Victoria','ROLE_SLT','$2a$10$zArhMpAXoIhDRvqmJSPI2uDJbGxoQft9g2LCa.7fBCOXDuqKVC6cS'),
-(3,1,'abimael.queirozl@gmail.com','Abimael','ROLE_SLT','$2a$10$zArhMpAXoIhDRvqmJSPI2uDJbGxoQft9g2LCa.7fBCOXDuqKVC6cS'),
-(4,1,'leandro@gmail.com','Leandro','ROLE_USER','$2a$10$zArhMpAXoIhDRvqmJSPI2uDJbGxoQft9g2LCa.7fBCOXDuqKVC6cS')
+(1,1,'admin@gmail.com','Admin','ROLE_ADMIN','$2a$10$zArhMpAXoIhDRvqmJSPI2uDJbGxoQft9g2LCa.7fBCOXDuqKVC6cS')
+-- (2,1,'vic@gmail.com','Victoria','ROLE_SLT','$2a$10$zArhMpAXoIhDRvqmJSPI2uDJbGxoQft9g2LCa.7fBCOXDuqKVC6cS'),
+-- (3,1,'abimael.queirozl@gmail.com','Abimael','ROLE_SLT','$2a$10$zArhMpAXoIhDRvqmJSPI2uDJbGxoQft9g2LCa.7fBCOXDuqKVC6cS'),
+-- (4,1,'leandro@gmail.com','Leandro','ROLE_USER','$2a$10$zArhMpAXoIhDRvqmJSPI2uDJbGxoQft9g2LCa.7fBCOXDuqKVC6cS')
 ;
 
 -- usuario: admin@gmail.com
